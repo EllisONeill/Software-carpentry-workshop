decb8b0e0d77e3d3615d172eabe3a2243934ed14
Ellis O'Neill <dr.ellis.oneill@gmail.com>
2014-10-21 14:21:40 -0700

    read fielname and outout with names

--- analyze_mosquito_data_script.py
+++ analyze_mosquito_data_script.py
@@ -1,13 +1,21 @@
+import sys
 import pandas as pd
 import analyze_mosquito_lib as mosquito_lib
 
-filename = "A1_mosquito_data.csv"
+
+filename = sys.argv[1]
+
+print "~"
+print "~"
+print "Analysing", filename
+print "~"
+print "~"
 
 data=pd.read_csv(filename)
 data["temperature"]=mosquito_lib.fahr_to_celsius(data["temperature"])
-parameters=mosquito_lib.analyze(data,"plot.png")
+parameters=mosquito_lib.analyze(data,filename.replace("csv","png"))
 
-parameters.to_csv("parameters.csv")
+parameters.to_csv(filename.replace("data", "parameters"))
 
 print parameters
 
